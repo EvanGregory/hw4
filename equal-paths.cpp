6bcc6548646e6f8b19e099cee1d8a0f6856cc4cb
Evan Gregory <ejgregor@usc.edu>
2023-03-14 00:00:46 +0000

    paths works

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,42 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int tHeight(Node* root)
+{
+    if (root == nullptr)
+    {
+        return 0;
+    }
+
+    int lh = tHeight(root->left);
+    int rh = tHeight(root->right);
+
+    if (lh >= rh)
+    {
+        return lh + 1;
+    }
+    else 
+    {
+        return rh + 1;
+    }
+}
+
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    if (root == nullptr) return true;
+    if (root->left != nullptr && root->right == nullptr)
+    {
+        return equalPaths(root->left);
+    }
+    if (root->left == nullptr && root->right != nullptr)
+    {
+        return equalPaths(root->right);
+    }
+    if (tHeight(root->left) == tHeight(root->right)) 
+        return true;
 
+    return false;
 }
+
 
